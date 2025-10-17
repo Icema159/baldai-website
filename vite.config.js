322f094ca89ee2e4bd85a3e16c9ac0d26a1b9f3e
Icema159 <aismantass@example.com>
2025-10-18 00:57:19 +0300

    added gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// base = tavo repo pavadinimas
 export default defineConfig({
   plugins: [react()],
+  base: "/project-name/", // pvz: /todo-app/, /link-hub/
 })
